f3b226f71362d593b4ae369283e7ae9f88f77dfd
Rudrank Riyam <rudrankriyam@gmail.com>
2024-10-15 13:33:20 +0530

    Add AgniKit package

---
+++ Package.swift
@@ -0,0 +1,25 @@
+// swift-tools-version: 6.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+  name: "AgniKit",
+  platforms: [
+    .iOS(.v16),
+    .macOS(.v13),
+    .tvOS(.v16),
+    .watchOS(.v9),
+    .visionOS(.v1)
+  ],
+  products: [
+    .library(
+      name: "AgniKit",
+      type: .static,
+      targets: ["AgniKit"]
+    )
+  ],
+  targets: [
+    .target(name: "AgniKit")
+  ]
+)
